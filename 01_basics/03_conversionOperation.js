bae789eb5868c223a0d154a92507a334eb47130a
Bhautikvekariya <127239928+Bhautikvekariya@users.noreply.github.com>
2024-09-02 10:07:13 +0000

    operationsconversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+// let someNumber=33
+
+// let StringNumber=String(someNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber);
+
+
+let someValue=0
+
+let BooleanValue=Boolean(someValue)
+console.log(BooleanValue);
+console.log(typeof BooleanValue)
+
+
+let someString="hitesh"
+
+let SomeNumber=Number(someString)
+console.log(SomeNumber);
+console.log(typeof SomeNumber)
