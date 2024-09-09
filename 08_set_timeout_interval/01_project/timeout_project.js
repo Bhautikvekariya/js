0981fac95047f726208ad316dbbf0f5021d95a4c
Bhautikvekariya <127239928+Bhautikvekariya@users.noreply.github.com>
2024-09-09 09:49:34 +0000

    change

---
+++ 08_set_timeout_interval/01_project/timeout_project.js
@@ -0,0 +1,21 @@
+let intervalId;
+
+function startInterval(){
+        clearInterval(intervalId);
+        intervalId=setInterval(()=>{
+             console.log("interval running");      
+        },1000);
+}
+
+function stopInterval(){
+    clearInterval(intervalId);
+    console.log(("interval stop"));
+}
+
+function restartInterval(){
+ stopInterval();  
+ startInterval();
+}
+document.querySelector("#start").addEventListener("click",startInterval);
+document.querySelector("#stop").addEventListener("click",stopInterval);
+document.querySelector("#restart").addEventListener("click",restartInterval);
